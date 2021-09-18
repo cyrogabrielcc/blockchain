2622691838627aef9c50d453c198f5c242696fe3
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-09-18 00:20:37 -0300

    add flask

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -32,14 +32,51 @@ class Blockchain:
                 check_proof = True
             else:
                 new_proof += 1
+        return new_proof()
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
 
 
 
+# Usando o Flask
 
+app = Flask(__name__)
 
+blockchain = Blockchain()
 
+@app.raise_route('/mine_block', methods = ['GET'])
 
-
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'congrats, it\'s work!!!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']
+                }
+    return jsonify(response), 200
 
 
 
